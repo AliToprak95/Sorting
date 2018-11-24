a1fe515e94b8eccefc910af02e2ac41252459ab5
Ali Toprak <45083841+AliToprak95@users.noreply.github.com>
2018-11-24 23:50:21 +0000

    Update SwitchSort.java

--- SwitchSort.java
+++ SwitchSort.java
@@ -1,18 +1,4 @@
-// MA407 EXERCISE 4
 // ALI TOPRAK
-// 201854644
-
-
-//Worst case running time of this algorithm is O(n^2)
-//Running time is worst when the array is decreasing array O(n^2)
-//Running time is best possible when the array is already sorted O(n)
-
-//Algorithm is terminates because in each iteration it sorts the part of array x[i] to end of the 
-//array and subtract 1 from i until it reaches the negative values
-//Algorithm is correct because if x[i] is greater than some of the elements in the array x[i+1] to end
-//then it moves these elements one left and place the x[i] to the right position.
-
-
 
 //!!!! TRUE BUT NOT INTENDED. IT HAS TO BE LIKE BUBBLE SORT
 
